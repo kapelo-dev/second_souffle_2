8b63d000e6719842b5806afae83bbae032bdc10a
kap-dev <sittijonathan99@gmail.com>
2025-05-12 14:19:16 +0000

    esperons1

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -20,35 +20,39 @@ export default function Navigation() {
             Second Souffle
           </Link>
 
-          {/* Navigation Links - Desktop Only */}
-          <div className="absolute hidden md:flex items-center justify-center left-1/2 transform -translate-x-1/2">
+          {/* Navigation Links - Large Screen Only */}
+          <div className="absolute hidden lg:flex items-center justify-center left-1/2 transform -translate-x-1/2">
             <Link href="/pourquoi-art" className="nav-link mx-4">Pourquoi l'art</Link>
             <Link href="/pourquoi-second-souffle" className="nav-link mx-4">Pourquoi Second Souffle</Link>
             <Link href="/nos-services" className="nav-link mx-4">Nos services</Link>
           </div>
 
-          {/* Right Side Links - Desktop Only */}
-          <div className="hidden md:flex items-center space-x-6">
+          {/* Right Side Links - Large Screen Only */}
+          <div className="hidden lg:flex items-center space-x-6">
             <Link href="/devenir-partenaire" className="nav-link">Devenir partenaire</Link>
             <Link href="/investir" className="btn-primary">INVESTIR</Link>
           </div>
 
-          {/* Hamburger - Mobile Only */}
+          {/* Hamburger - Small Screen Only */}
           <button
-            className="block md:hidden w-8 h-8"
+            className="flex lg:hidden flex-col justify-center items-center w-8 h-8"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             aria-label="Menu"
           >
-            <div className="w-6 h-5 mx-auto flex flex-col justify-between">
-              <span className={`block h-0.5 w-full bg-black transform transition-transform ${isMenuOpen ? 'rotate-45 translate-y-2' : ''}`} />
-              <span className={`block h-0.5 w-full bg-black transition-opacity ${isMenuOpen ? 'opacity-0' : ''}`} />
-              <span className={`block h-0.5 w-full bg-black transform transition-transform ${isMenuOpen ? '-rotate-45 -translate-y-2' : ''}`} />
-            </div>
+            <span className={`w-6 h-0.5 bg-black transition-all ${
+              isMenuOpen ? 'rotate-45 translate-y-2' : ''
+            }`}></span>
+            <span className={`w-6 h-0.5 bg-black transition-all my-1 ${
+              isMenuOpen ? 'opacity-0' : ''
+            }`}></span>
+            <span className={`w-6 h-0.5 bg-black transition-all ${
+              isMenuOpen ? '-rotate-45 -translate-y-2' : ''
+            }`}></span>
           </button>
 
           {/* Mobile Menu */}
           {isMenuOpen && (
-            <div className="absolute top-full left-0 right-0 bg-custom-bg border-t border-[#E5E5E5] md:hidden">
+            <div className="absolute top-full left-0 right-0 bg-custom-bg border-t border-[#E5E5E5] lg:hidden">
               <div className="px-4 py-4 space-y-4">
                 <Link href="/pourquoi-art" className="block nav-link">Pourquoi l'art</Link>
                 <Link href="/pourquoi-second-souffle" className="block nav-link">Pourquoi Second Souffle</Link>
